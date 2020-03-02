b7f8893543d834757cd1c10266911d89eb9ca55e
Blake Cox <blakecox@Blakes-MBP.attlocal.net>
2020-03-02 11:21:12 -0600

    Attempting to push to master, forgot to make an initial branch

---
+++ EmployeeApp/EmployeeApp/alertPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace EmployeeApp
+{
+    public partial class alertPage : ContentPage
+    {
+        public alertPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
